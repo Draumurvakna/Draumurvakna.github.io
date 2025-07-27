752b777fad1264922ac6310a0a43b2c551aa1800
Draumurvakna <guanshengyuanlu@163.com>
2025-07-27 23:18:22 +0800

    更新

--- js/light-dark-switch.js
+++ js/light-dark-switch.js
@@ -1,15 +1,9 @@
 let themeFunc = async function() {
-    let btn = document.getElementById("theme-btn");
     let root = document.documentElement;
-    btn.addEventListener('click', e => {
-        if (root.getAttribute('theme') == 'dark') {
-            root.setAttribute('theme', 'light');
-            localStorage.setItem('theme', 'light');
-        } else {
-            root.setAttribute('theme', 'dark');
-            localStorage.setItem('theme', 'dark');
-        }
-    });
+    
+    // 强制设置为白天模式（已移除主题切换功能）
+    root.setAttribute('theme', 'light');
+    localStorage.setItem('theme', 'light');
 };
 themeFunc();
 
